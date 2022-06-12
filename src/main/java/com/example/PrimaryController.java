72eb4c889bb2fb877c1fd5a17c0e9283a15311c3
raed charrad <raed.charrad91@gmail.com>
2022-06-12 18:18:33 +0100

    add Lagin function

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -2,11 +2,29 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private Label error;
+
+    @FXML
+    public void Login() {
+        if (username.getText().equals("admin") && password.getText().equals("admin")) {
+            try {
+                App.setRoot("Secondary");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            error.setText("Invalid username or password");
+        }
     }
+
 }
